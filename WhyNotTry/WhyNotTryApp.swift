e1cf34916b47ed5077983538ace69581bfdcdb23
Gishkori <gishkori@hotmail.com>
2023-10-19 22:05:09 +1100

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Ali Asghar Khan Gishkori on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
