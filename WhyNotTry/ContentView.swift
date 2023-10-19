2d06e8f829d2bd8b40713c43c20e92a2049deec4
Gishkori <gishkori@hotmail.com>
2023-10-19 23:06:53 +1100

    play random sports app

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State private var selected = "Baseball"
+    
+    @State private var id = 1
+
+    
     var body: some View {
-            Text("Hello, world!")
+        VStack {
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            
+            Spacer()
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                    .transition(.slide)
+                    .id(id)
+                
+                Text("\(selected)!")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+            
+            Spacer()
+            
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
+
+            
     }
 }
 
